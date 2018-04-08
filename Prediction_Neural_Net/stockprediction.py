f0be239984282008934c67886907bf15d7fdd8d5
Jerry Peng <png.jrry@gmail.com>
2018-04-08 01:12:06 -0400

    adjusted dropout rate to .5

--- Prediction_Neural_Net/stockprediction.py
+++ Prediction_Neural_Net/stockprediction.py
@@ -79,7 +79,7 @@ hidden_2 = tf.nn.relu(tf.add(tf.matmul(hidden_1, W_hidden_2), bias_hidden_2))
 hidden_3 = tf.nn.relu(tf.add(tf.matmul(hidden_2, W_hidden_3), bias_hidden_3))
 hidden_4 = tf.nn.relu(tf.add(tf.matmul(hidden_3, W_hidden_4), bias_hidden_4))
 
-keep_prob = tf.placeholder(tf.float32)
+keep_prob = tf.placeholder(5e-1)
 h_fc1_drop = tf.nn.dropout(hidden_4, keep_prob)
 
 # Output layer (transpose!)
